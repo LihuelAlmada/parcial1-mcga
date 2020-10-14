6fe08c6200c9f17059e9d5a2e092ea68dda1f010
Necralma <lihuel.almada@gmail.com>
2020-10-14 15:07:38 -0300

    Updates

---
+++ precache-manifest.0bba9b3453ce94ae7ec377021212889c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0099bb32027d5193736ccdc2746e06f7",
+    "url": "/parcial1-mcga/index.html"
+  },
+  {
+    "revision": "f20e8cd677177e5f6fff",
+    "url": "/parcial1-mcga/static/css/main.2b08fd03.chunk.css"
+  },
+  {
+    "revision": "806d955550d8bd526206",
+    "url": "/parcial1-mcga/static/js/2.44f374c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/parcial1-mcga/static/js/2.44f374c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20e8cd677177e5f6fff",
+    "url": "/parcial1-mcga/static/js/main.c4ef0439.chunk.js"
+  },
+  {
+    "revision": "789a33f59683f2d94d84",
+    "url": "/parcial1-mcga/static/js/runtime-main.51c99a7a.js"
+  }
+]);
